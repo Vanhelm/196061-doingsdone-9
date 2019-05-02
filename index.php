f259c5bbe817c60823acc2bf12d85df6d5829144
Максим <mpohilchuk@gmail.com>
2019-05-03 02:13:26 +0500

    get запросы

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <?php
+header("HTTP/1.0 404 Not Found");
 require_once('system/init.php');
 
-$show_complete_tasks = rand(0, 1);
-
-
-/*$get_tasks_user = "SELECT name, status, term FROM tasks WHERE id_user = 1";
-$res_task = mysqli_query($link, $get_tasks_user);
-$tasks = mysqli_fetch_all($res_task, MYSQLI_ASSOC);*/
-
+$show_complete_tasks = rand(0, 1);	
+$content_page;
+if(empty($tasks) or empty($projects))
+{
+	http_response_code(404);
+	//почему-то не подключает
+	$content_page = include_template('/system/error404.php');
+}
+else{
 $content_page = include_template('index.php', ['tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
+}
+
 $layout_content = include_template('layout.php',[
     'projects' => $projects,
     'tasks' => $tasks,
     'content' => $content_page,
-    'title' => $title
+    'title' => $title,
+    'active' => $active_project_id
 ]);
 
 print ($layout_content);
--- system/init.php
+++ system/init.php
@@ -2,12 +2,13 @@
 ini_set('error_reporting', E_ALL); 
 ini_set('display_errors', 1); 
 ini_set('display_startup_errors', 1);
-require_once('functions/function.php');
+require_once('functions/functions.php');
 require_once('helpers.php');
 
 date_default_timezone_set('Europe/Moscow');
 $title = "Дела в порядке";
-
+$active_project_id = 0;
+$user_id = 2;
 
 $link = mysqli_connect("localhost", "root", "", "affairs_order");
 mysqli_set_charset($link, "utf8");
@@ -18,36 +19,14 @@ if(!$link)
 	exit();
 }
 
-$get_projects_user = "SELECT p.project_id AS project_id, p.name AS project_name, COUNT(t.project_id) AS count_item  FROM projects p LEFT JOIN tasks t ON p.project_id = t.project_id WHERE t.id_user = 1 AND t.status=0 GROUP BY p.project_id";
+$get_projects_user = "SELECT p.project_id AS project_id, p.name AS project_name, COUNT(t.project_id) AS count_item  FROM projects p LEFT JOIN tasks t ON p.project_id = t.project_id WHERE p.id_user = 2 GROUP BY p.project_id";
 $res = mysqli_query($link, $get_projects_user);
 $projects = mysqli_fetch_all($res, MYSQLI_ASSOC);
 
-function get_url_project($projectsID, $projectsName)
+if(isset($_GET['id']))
 {
-	$param = $_GET;
-	$param['id'] = $projectsID;
-	$param['name'] = $projectsName;
-	$query = http_build_query($param);
-	$url__project = "/" . "?" . $query;
-	return $url__project;
+	$active_project_id = intval($_GET['id']);
 }
 
-function call_task ($link)
-{
-	if(isset($_GET['id']) AND isset($_GET['name']))
-	{
-		$get_call_task = "SELECT name, status, term FROM tasks WHERE project_id=2 AND id_user=1";
-		$res = mysqli_query($link, $get_call_task);
-		$tasks = mysqli_fetch_all($res, MYSQLI_ASSOC);
+$tasks = get_task($link, $active_project_id, $user_id);
 
-		return $tasks;
-	}
-
-	$get_tasks_user = "SELECT name, status, term FROM tasks WHERE id_user=1";
-	$res_task = mysqli_query($link, $get_tasks_user);
-	$tasks = mysqli_fetch_all($res_task, MYSQLI_ASSOC);
-
-	return $tasks;
-}
-
-$tasks = call_task($link);
